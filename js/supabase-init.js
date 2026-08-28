b6af4a6f8e2d8487bdcce2be585f11177b27f526
Alshrefdevl <hamodealshref2005@gmail.com>
2026-08-28 11:55:36 +0300

    Initialize Supabase client in supabase-init.js

--- js/supabase-init.js
+++ js/supabase-init.js
@@ -1,1 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
+const supabaseUrl = 'https://kkmnncumzhcifagiqiua.supabase.co';
+const supabaseKey = 'sb_publishable_VFMfrWj6UhfoI-C5GnLyaA_iTly29Zb';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
